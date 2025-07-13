db513221b628d8e91ec43aa91c58579993fd61bd
suad alhodifi <suadtech1@gmail.com>
2025-07-13 16:29:15 +0000

    Fix Cloudinary for Heroku deployment

--- eco_blog/settings.py
+++ eco_blog/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1ka@327#o&h9u=$lq+4o#t$uxerdp=(bs_p&@)xm6xcs%2iot+')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=True
 
 
 INSTALLED_APPS = [
